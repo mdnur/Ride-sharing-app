08c0d751c732b80b001558371c50eecd3b6ea54c
Mohammad Nur <mdnur701@gmail.com>
2023-06-18 23:36:58 +0600

    MainTable class added where all the common database operation has added

---
+++ lib/MainTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace lib;
+
+abstract class MainTable
+{
+    protected  $table;
+
+    abstract public function Insert();
+
+    abstract public function update($id);
+
+
+    public function delete($id) {
+        $sql ="DELETE FROM ".$this->table." WHERE id=:id";
+        $stmt =Database::prepare($sql);
+        $stmt->bindParam(":id",$id );
+        return $stmt->execute();
+    }
+
+
+    public function readByid($id) {
+        $sql ="SELECT * FROM ".$this->table." WHERE id =:id";
+        $stmt =Database::prepare($sql);
+        $stmt->bindParam(":id",$id);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+    public function readAll() {
+        $sql ="SELECT * FROM ".$this->table." ORDER BY id DESC ";
+        $stmt =Database::prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+}
