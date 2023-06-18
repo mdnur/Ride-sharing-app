ac747e902f4e855fabf60347c6292678bf865176
Mohammad Nur <mdnur701@gmail.com>
2023-06-18 23:23:40 +0600

    Config File added wherer all the database related configuration will be done

---
+++ config/Config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Config
+ * @author MD Nur
+ */
+const DB_HOST = "127.0.0.1";
+const DB_NAME = "";
+const DB_USER = "";
+const DB_PASS = "";
+const PROJECT_ROOT_PATH = __DIR__;
