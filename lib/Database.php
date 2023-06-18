142a1d222393bcfa516d13de17ec948d07feada5
Mohammad Nur <mdnur701@gmail.com>
2023-06-18 23:33:08 +0600

    Database class created where all the database connection function written

---
+++ lib/Database.php
@@ -0,0 +1,25 @@
+<?php
+//require_once '../config/Config.php';
+
+
+class Database {
+    private static $pdo;
+
+    public static function Connection(): PDO
+    {
+        if(!isset(self::$pdo)){
+            try {
+                self::$pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASS);
+            } catch (PDOException $e) {
+                echo "Connection fail...".$e->getMessage();
+            }
+        }
+        return self::$pdo;
+    }
+
+    public static function prepare($sql): false|PDOStatement
+    {
+        return self::Connection()->prepare($sql);
+    }
+
+}
