1e8494156bc5b0d61ea5d1e9a2bd5dc0dc867342
Mohammad Nur <mdnur701@gmail.com>
2023-07-13 13:30:11 +0600

    header and footer added

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<?php
+include_once "lib/Session.php";
+
+use lib\Session;
+
+Session::checkSession();
+spl_autoload_register(function ($class) {
+    include "Classes/" . $class . ".php";
+});
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,33 +1,11 @@
-<?php
-include_once "lib/Session.php" ;
-use lib\Session;
+<?php include_once "inc/header.php"; ?>
+    <?php
+    if (isset(($_GET['action']))) {
+        if ($_GET['action'] == 'logout')
+            Session::destory();
+        header("Location:login.php");
+    }
+    ?>
+    <a href="?action=logout">LOGOUT</a>
 
-Session::checkSession();
-spl_autoload_register(function($class){
-    include "Classes/".$class.".php";
-});
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<!--<form action="" method="get">-->
-<!--    <input type="submit" value="Logout" name="logout">-->
-<!--</form>-->
-
-<?php
-if(isset(($_GET['action']))){
-    if($_GET['action'] == 'logout')
-    Session::destory();
-    header("Location:login.php");
-}
-?>
-<a href="?action=logout">LOGOUT</a>
-</body>
-</html>
+<?php include_once "inc/footer.php"; ?>
