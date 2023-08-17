5b8c1bfc7fb8dea12e5eb7cfc653403a5d7ca71a
Mohammad Nur <mdnur701@gmail.com>
2023-08-18 05:31:23 +0600

    Session init function modify with PHP_SESSION_NONE

--- lib/Session.php
+++ lib/Session.php
@@ -2,70 +2,90 @@
 
 namespace lib;
 
-class Session{
+date_default_timezone_set("Asia/Dhaka");
+
+class Session
+{
 
 
     public static function init(): void
     {
-        session_start();
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        // session_start();
     }
 
-    public static function set($key,$value): void
+    public static function set($key, $value): void
     {
-        $_SESSION[$key] =$value;
+        $_SESSION[$key] = $value;
     }
 
 
-    public static function get ($key) {
+    public static function get($key)
+    {
         return $_SESSION[$key] ?? FALSE;
     }
 
     public static function checkSession(): void
     {
+
         self::init();
-        if(!self::get("login")){
-            self:: destory();
-           header("location:login.php");
+        if (!self::get("login")) {
+            self::destory();
+            header("location:login.php");
         }
     }
     public static function CheckAdminSession(): void
     {
         self::init();
-        if(!self::get("adminLogin")){
-            self:: destory();
-           header("location:login.php");
+        if (!self::get("adminLogin")) {
+            self::destory();
+            header("location:login.php");
         }
     }
 
-    public static function checkLogin() {
+    public static function ifLogin()
+    {
+        if (!self::get("login")) {
+            self::init();
+        }
+        if (self::get("login")) {
+            return true;
+        }
+    }
+    public static function checkLogin()
+    {
         self::init();
-        if(self::get("login")){
-           header("location:index.php");
+        if (self::get("login")) {
+            header("location:home.php");
         }
     }
 
     public static function CheckDriverSession(): void
     {
         self::init();
-        if(!self::get("driverLogin")){
-            self:: destory();
-           header("location:login.php");
+        if (!self::get("driverLogin")) {
+            self::destory();
+            header("location:login.php");
         }
     }
-    public static function checkDriverLogin() {
+    public static function checkDriverLogin()
+    {
         self::init();
-        if(self::get("driverLogin")){
-           header("location:index.php");
+        if (self::get("driverLogin")) {
+            header("location:index.php");
         }
     }
 
 
-    
 
-    public static function checkAdminLogin() {
+
+    public static function checkAdminLogin()
+    {
         self::init();
-        if(self::get("adminLogin")){
-           header("location:index.php");
+        if (self::get("adminLogin")) {
+            header("location:index.php");
         }
     }
 
@@ -73,11 +93,11 @@ class Session{
     {
         session_destroy();
         session_unset();
-//        header("Location:login.php?action=logout");
+        //        header("Location:login.php?action=logout");
     }
 
-    public static function getName($role){
+    public static function getName($role)
+    {
         return Session::get($role)['name'];
     }
-
 }
