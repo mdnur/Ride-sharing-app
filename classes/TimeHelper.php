12ceb9c171582967da8afd7a0689ac4c32d18464
Mohammad Nur <mdnur701@gmail.com>
2023-08-27 01:32:45 +0600

    some bugs has fixed in driver functionality

--- classes/TimeHelper.php
+++ classes/TimeHelper.php
@@ -12,7 +12,7 @@ class TimeHelper
     public static function getFormattedTime1($time)
     {
         $timestamp = strtotime($time);
-        return date('M-y H:i', $timestamp);
+        return date('d-M H:i', $timestamp);
     }
 
     public static function getFormattedTime3($time)
@@ -38,6 +38,24 @@ class TimeHelper
         }
         return true;
     }
+    public static function checkTimeDiff($date)
+    {
+        // Get the current time in Unix timestamp format.
+        $currentTime = time();
+
+        // Get the booking time in Unix timestamp format.
+        $bookingTime = strtotime($date);
+
+        // Calculate the difference between the current time and the booking time.
+        $difference = $bookingTime - $currentTime;
+
+        // If the difference is less than 10 minutes (600 seconds), then disable the cancel button.
+        if ($difference < 600) {
+            return false;
+        }
+        return true;
+    }
+
 
     public static function getFormattedTime4($time)
     {
